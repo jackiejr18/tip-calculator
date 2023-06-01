46cca6bcfa46e4045ba9ee72967569a4e1b807ea
Jackie Jr <jwysinger@gmail.com>
2023-06-01 11:04:19 -0500

    set the default vaulues for all the calc fields

--- tipcalculator.js
+++ tipcalculator.js
@@ -6,3 +6,10 @@ const totalAmount = document.getElementById("total-amount");
 const totalTip = document.getElementById("calculate");
 const resetTip = document.getElementById("reset");
 const errorTip = document.getElementById("error");
+
+// set the default value on load
+billEl.value = "0";
+tipEl.value = "0";
+peopleEl.value = "1";
+tipAmount.innerHTML = "$" + +0.0;
+totalAmount.innerHTML = "$" + +(0.0).toFixed(2);
